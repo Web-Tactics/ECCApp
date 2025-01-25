a2b2aab81816455142841cd4ed0f0baa4c2944d3
Howard Warren <howardwarren89@gmail.com>
2025-01-25 02:59:40 +0000

    udate starter page txt and app.json

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -11,7 +11,7 @@ export default function StartScreen({ navigation }) {
       <Logo />
       <Header>Welcome to the Mobile Emergency Card Creator</Header>
       <Paragraph>
-      This app creates up to 6 contact cards from your phone's contact list and allows you to print them, providing a handy backup to keep in your wallet for times when your phone isn't accessible.
+      This app creates up to 6 contact cards from your phone's contact list and allows you to print them. The purpose is to provide handy backups to keep in your wallet for times when your phone isn't accessible.
       </Paragraph>
       <Button
         mode="contained"
